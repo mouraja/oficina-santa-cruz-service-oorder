06024c9ce1b62ea4ecd042b1c9edfde33860ccc5
Joao Augusto <mouraja@gmail.com>
2021-04-15 08:46:51 -0300

    Criando DTO Manager - Public Vehicle Manager 15/04 as 09h

--- src/main/resources/static/PublicClients.js
+++ src/main/resources/static/PublicClients.js
@@ -1,165 +1,160 @@
-//var publicClients = [
-  /*{id: 1, name: 'Laravel', description: 'Provide Laravel Information.', price: 100},
-  {id: 2, name: 'AngularJS', description: 'Provide AngularJS Information.', price: 100},
-  {id: 3, name: 'PHP', description: 'Provide PHP Information.', price: 100}*/
-//];
 
 publicClients: [];
-            
-function findPublicClient (id) {
-  //return this.publicClients[findPublicClientKey(id)];
-  return findPublicClientKey(id)[0];
+
+function findPublicClient(id) {
+    return findPublicClientKey(id);
+}
+;
+
+function findPublicClientKey(id) {
+    return this.publicClients.find(key => key.id === id);
+}
+;
+
+const PublicClientService = {
+
+    base_url: 'http://localhost:8080/api/manager/client/public/',
+
+    findAll(fn) {
+        axios
+                .get(this.base_url, {
+                    'headers': {
+                        "Content-type": "application/json"
+                    }
+                })
+                .then(response => fn(response))
+                .catch(error => console.log(error));
+    },
+
+    findById(id, fn) {
+        axios
+                .get(this.base_url + id, {
+                    'headers': {
+                        "Content-type": "application/json"
+                    }
+                })
+                .then(response => fn(response))
+                .catch(error => console.log(error));
+    },
+
+    create(publicClient, fn) {
+        axios
+                .post(this.base_url, publicClient)
+                .then(response => fn(response))
+                .catch(error => console.log(error));
+    },
+
+    update(id, publicClient, fn) {
+        axios
+                .put(this.base_url + id, publicClient)
+                .then(response => fn(response))
+                .catch(error => console.log(error));
+    },
+
+    delete(id, fn) {
+        axios
+                .delete(this.base_url + id)
+                .then(response => fn(response))
+                .catch(error => console.log(error));
+    },
+
 };
 
-function findPublicClientKey (id) {
-  //console.log(id + "-" + this.publicClients.filter(key => key.id === id));
-  return this.publicClients.filter( key => key.id === id );
-  /*
-  for (var key = 0; key < this.publicClients.length; key++) {
-    if (this.publicClients[key].id === id) {
-      return key;
+const publicClientList = Vue.extend({
+    template: '#public-client-list',
+    data: function () {
+        return {publicClients: [], searchKey: ''};
+    },
+    computed: {
+        filteredPublicClients() {
+            return this.publicClients.filter((publicClient) => {
+                return publicClient.publicName.indexOf(this.searchKey) > -1
+                        || publicClient.primaryContact.indexOf(this.searchKey) > -1
+                        || publicClient.primaryEmail.indexOf(this.searchKey) > -1
+                        || publicClient.publicFantasyName.toString().indexOf(this.searchKey) > -1;
+            });
+        }
+    },
+    mounted() {
+        PublicClientService.findAll(r => {
+            this.publicClients = r.data;
+            publicClients = r.data
+        })
     }
-  }
-  */
-};
-
-var PublicClientService = {
-
-  api_path: 'http://localhost:8080/api/manager/client/public/',
-
-  findAll(fn) {
-    axios
-      .get(this.api_path, {
-          'headers': {
-            "Content-type": "application/json"
-          }
-      })
-      .then(response => fn(response))
-      .catch(error => console.log(error));
-  },
-
-  findById(id, fn) {
-    axios
-      .get(this.api_path + id)
-      .then(response => fn(response))
-      .catch(error => console.log(error));
-  },
-
-  create(publicClient, fn) {
-    axios
-      .post(this.api_path, publicClient)
-      .then(response => fn(response))
-      .catch(error => console.log(error));
-  },
-
-  update(id, publicClient, fn) {
-    axios
-      .put(this.api_path + id, publicClient)
-      .then(response => fn(response))
-      .catch(error => console.log(error));
-  },
-
-  delete(id, fn) {
-    axios
-      .delete(this.api_path + id)
-      .then(response => fn(response))
-      .catch(error => console.log(error));
-  },
-
-};
-
-var publicClientList = Vue.extend({
-  template: '#public-client-list',
-  data: function () {
-    return {publicClients: [], searchKey: ''};
-  },
-  computed: {
-    filteredPublicClients() {
-      return this.publicClients.filter((publicClient) => {
-      	return publicClient.publicName.indexOf(this.searchKey) > -1
-      	  || publicClient.primaryContact.indexOf(this.searchKey) > -1
-      	  || publicClient.primaryEmail.indexOf(this.searchKey) > -1
-      	  || publicClient.publicFantasyName.toString().indexOf(this.searchKey) > -1;
-      });
-    }
-  },
-  mounted() {
-    PublicClientService.findAll(r => {this.publicClients = r.data; publicClients = r.data})
-  }
 });
 
 var publicClientView = Vue.extend({
-  template: '#public-client-view',
-  data: function () {
-    return {publicClient: findPublicClient(this.$route.params.publicClient_id)};
-  }
+    template: '#public-client-view',
+    data: function () {
+        return {publicClient: findPublicClient(this.$route.params.publicClient_id)};
+    }
 });
 
 var publicClientEdit = Vue.extend({
-  template: '#public-client-edit',
-  data: function () {
-    return {publicClient: findPublicClient(this.$route.params.publicClient_id)};
-  },
-  methods: {
-    updatePublicClient: function () {
-      PublicClientService.update(this.publicClient.id, this.publicClient, r => router.push('/'));
+    template: '#public-client-edit',
+    data: function () {
+        return {publicClient: findPublicClient(this.$route.params.publicClient_id)};
+    },
+    methods: {
+        updatePublicClient: function () {
+            PublicClientService.update(this.publicClient.id, this.publicClient, r => router.push('/'));
+        }
     }
-  }
 });
 
 var publicClientDelete = Vue.extend({
-  template: '#public-client-delete',
-  data: function () {
-    return {publicClient: findPublicClient(this.$route.params.publicClient_id)};
-  },
-  methods: {
-    deletePublicClient: function () {
-      PublicClientService.delete(this.publicClient.id, r => router.push('/'));
+    template: '#public-client-delete',
+    data: function () {
+        return {publicClient: findPublicClient(this.$route.params.publicClient_id)};
+    },
+    methods: {
+        deletePublicClient: function () {
+            PublicClientService.delete(this.publicClient.id, r => router.push('/'));
+        }
     }
-  }
 });
 
 var publicClientAdd = Vue.extend({
-  template: '#public-client-add',
-  data: function () {
-    return {
-      publicClient: {
-        publicName: '',
-        publicFantasyName: '',
-        addressDelivery: '',
-        addressBilling: '',
-        primaryContact: '',
-        primaryEmail: '',
-        primaryPhoneNumber: '',
-        primarySupportContact: '',
-        observations: '',
-        status: false
-      }
+    template: '#public-client-add',
+    data: function () {
+        return {
+            publicClient: {
+                publicName: '',
+                publicFantasyName: '',
+                addressDelivery: '',
+                addressBilling: '',
+                primaryContact: '',
+                primaryEmail: '',
+                primaryPhoneNumber: '',
+                primarySupportContact: '',
+                observations: '',
+                status: false
+            }
+        }
+    },
+    methods: {
+        createPublicClient() {
+            PublicClientService.create(this.publicClient, r => router.push('/'));
+        }
     }
-  },
-  methods: {
-    createPublicClient() {
-      PublicClientService.create(this.publicClient, r => router.push('/'));
-    }
-  }
 });
 
 var router = new VueRouter({
-  routes: [
-    {path: '/', component: publicClientList},
-    {path: '/public-client/view/:publicClient_id', component: publicClientView, name: 'public-client-view'},
-    {path: '/public-client/add', component: publicClientAdd, name: 'public-client-add'},
-    {path: '/public-client/edit/:publicClient_id', component: publicClientEdit, name: 'public-client-edit'},
-    {path: '/public-client/delete/:publicClient_id', component: publicClientDelete, name: 'public-client-delete'}
-  ]
+    routes: [
+        {path: '/', component: publicClientList},
+        {path: '/public-client/view/:publicClient_id', component: publicClientView, name: 'public-client-view'},
+        {path: '/public-client/add', component: publicClientAdd, name: 'public-client-add'},
+        {path: '/public-client/edit/:publicClient_id', component: publicClientEdit, name: 'public-client-edit'},
+        {path: '/public-client/delete/:publicClient_id', component: publicClientDelete, name: 'public-client-delete'}
+    ]
 });
 
 new Vue({
-  el: '#app',
-  data: {
-    company: "Oficina Santa Cruz",
-    title: "Public Client Managment",
-    subtitle: "" 
-  },
-  router: router
+    el: '#app',
+    data: {
+        company: "Oficina Santa Cruz",
+        title: "Public Client Managment",
+        subtitle: ""
+    },
+    router: router
 });
--- src/main/resources/static/vehicles.js
+++ src/main/resources/static/vehicles.js
@@ -1,168 +1,185 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-var vehicles = [];
-var clients = [];
-var base_path = '/api/vehicles';
-var base_client = '/api/client/public';
-function findVehicle (id) {
-  return vehicles[findVehicleKey(id)];
+
+vehicles: [];
+
+function findVehicle(id) {
+    return findVehicleKey(id);
+}
+;
+
+function findVehicleKey(id) {
+    return (this.vehicles.find(key => key.id === id));
 }
 
-function findVehicleKey (id) {
-  for (var key = 0; key < vehicles.length; key++) {
-    if (vehicles[key].id === id) {
-      return key;
-    }
-  }
-}
+const VehicleService = {
+    base_url: 'http://localhost:8080/api/manager/vehicle/',
+    base_client: 'http://localhost:8080/api/manager/client/public/',
 
-var vehicleService = {
-  findAll(fn) {
-    axios
-      .get(base_path)
-      .then(response => fn(response))
-      .catch(error => console.log(error));
-  },
+    findAll(fn) {
+        axios
+                .get(this.base_url, {
+                    'headers': {
+                        "Content-type": "application/json"
+                    }
+                })
+                .then(response => fn(response))
+                .catch(error => console.log(error));
+    },
 
-  findById(id, fn) {
-    axios
-      .get(base_path + id)
-      .then(response => fn(response))
-      .catch(error => console.log(error));
-  },
+    findById(id, fn) {
+        axios
+                .get(this.base_url + id, {
+                    'headers': {
+                        "Content-type": "application/json"
+                    }
+                })
+                .then(response => fn(response))
+                .catch(error => console.log(error));
+    },
 
-  create(vehicle, fn) {
-    axios
-      .post(base_path, vehicle)
-      .then(response => fn(response))
-      .catch(error => console.log(error));
-  },
+    create(vehicle, fn) {
+        axios
+                .post(this.base_url, vehicle)
+                .then(response => fn(response))
+                .catch(error => console.log(error));
+    },
 
-  update(id, vehicle, fn) {
-    axios
-      .put(base_path + id, vehicle)
-      .then(response => fn(response))
-      .catch(error => console.log(error));
-  },
+    update(id, vehicle, fn) {
+        axios
+                .put(this.base_url + id, vehicle)
+                .then(response => fn(response))
+                .catch(error => console.log(error));
+    },
 
-  deleteVehicle(id, fn) {
-    axios
-      .delete(base_path + id)
-      .then(response => fn(response))
-      .catch(error => console.log(error));
-  },
+    delete(id, fn) {
+        axios
+                .delete(this.base_url + id)
+                .then(response => fn(response))
+                .catch(error => console.log(error));
+    },
 
-  listClients(fn) {
-    axios
-      .get(base_client)
-      .then(response => fn(response))
-      .catch(error => console.log(error));
-  }
+    findClients(fn) {
+        axios
+                .get(this.base_client, {
+                    'headers': {
+                        "Content-type": "application/json"
+                    }
+                })
+                .then(response => fn(response))
+                .catch(error => console.log(error));
+    },
 
 };
 
-var List = Vue.extend({
-  template: '#vehicle-list',
-  data: function () {
-    return {vehicles: [], searchKey: ''};
-  },
-  computed: {
-    filteredVehicles() {
-        console.log(this.vehicles.length);
-      return this.vehicles.filter((vehicle) => {
-      	return vehicle.licensePlate.indexOf(this.searchKey) > -1
-            || vehicle.manufactor.indexOf(this.searchKey) > -1
-            || vehicle.model.indexOf(this.searchKey) > -1;
-      });
+const VehicleList = Vue.extend({
+    template: '#vehicle-list',
+    data: function () {
+        return {vehicles: [], searchKey: ''};
+    },
+    computed: {
+        filteredVehicles() {
+            return this.vehicles.filter((vehicle) => {
+                return vehicle.licensePlate.indexOf(this.searchKey) > -1
+                        || vehicle.manufactor.indexOf(this.searchKey) > -1
+                        || vehicle.model.indexOf(this.searchKey) > -1;
+            });
+        }
+    },
+    mounted() {
+        VehicleService.findAll(r => {
+            this.vehicles = r.data;
+            vehicles = r.data
+        })
     }
-  },
-  mounted() {
-    vehicleService.findAll(r => {this.vehicles = r.data; vehicles = r.data})
-  }
 });
 
-var Vehicle = Vue.extend({
-  template: '#vehicle',
-  data: function () {
-    return {vehicle: findVehicle(this.$route.params.vehicle_id)};
-  }
-});
-
-var VehicleEdit = Vue.extend({
-  template: '#vehicle-edit',
-  data: function () {
-    return {vehicle: findVehicle(this.$route.params.vehicle_id)};
-  },
-  methods: {
-    updateVehicle: function () {
-      vehicleService.update(this.vehicle.id, this.vehicle, r => router.push('/'))
+const VehicleView = Vue.extend({
+    template: '#vehicle-view',
+    data: function () {
+        return {vehicle: findVehicle(this.$route.params.vehicle_id)};
     }
-  }
 });
 
-var VehicleDelete = Vue.extend({
-  template: '#vehicle-delete',
-  data: function () {
-    return {vehicle: findVehicle(this.$route.params.vehicle_id)};
-  },
-  methods: {
-    deleteVehicle: function () {
-      vehicleService.deleteVehicle(this.vehicle.id, r => router.push('/'));
+const VehicleEdit = Vue.extend({
+    template: '#vehicle-edit',
+    data: function () {
+        return {vehicle: findVehicle(this.$route.params.vehicle_id)};
+    },
+    methods: {
+        updateVehicle: function () {
+            VehicleService.update(this.vehicle.id, this.vehicle, r => router.push('/'))
+        }
     }
-  }
 });
 
-var AddVehicle = Vue.extend({
-  template: '#add-vehicle',
-  data() {
-    return {
-      vehicle: {
-        manufactor: '',
-        model: '',
-        madeYear: '',
-        modelYear: '',
-        licensePlate: '',
-        clientOwner: 0,
-        observations: '',
-        status: true
-      }
-    };
-  },
-  methods: {
-    createVehicle() {
-      vehicleService.create(this.vehicle, r => router.push('/'));
+const VehicleDelete = Vue.extend({
+    template: '#vehicle-delete',
+    data: function () {
+        return {vehicle: findVehicle(this.$route.params.vehicle_id)};
+    },
+    methods: {
+        deleteVehicle: function () {
+            VehicleService.deleteVehicle(this.vehicle.id, r => router.push('/'));
+        }
     }
-  }
 });
 
-var router = new VueRouter({
-	routes: [
-		{path: '/', component: List},
-		{path: '/vehicle/:vehicle_id', component: Vehicle, name: 'vehicle'},
-		{path: '/add-vehicle', component: AddVehicle},
-		{path: '/vehicle/:vehicle_id/edit', component: VehicleEdit, name: 'vehicle-edit'},
-		{path: '/vehicle/:vehicle_id/delete', component: VehicleDelete, name: 'vehicle-delete'}
-	]
-});
-
-var ListClients = new Vue({
-  el: 'add-clientOwner',
-  data: () => ({
-    clients: []
-  }),
-  methods: {
-    async getClients () {
-      await vehicleService.listClients(r => {this.clients = r.data; clients = r.data})
+const VehicleAdd = Vue.extend({
+    template: '#add-vehicle',
+    data() {
+        return {
+            vehicle: {
+                manufactor: '',
+                model: '',
+                madeYear: '',
+                modelYear: '',
+                licensePlate: '',
+                clientOwner: 0,
+                observations: '',
+                status: false
+            }
+        }
+    },
+    methods: {
+        createVehicle() {
+            VehicleService.create(this.vehicle, r => router.push('/'));
+        }
     }
-  },
-  created () {
-    this.getClients
-  }
+});
+
+const router = new VueRouter({
+    routes: [
+        {path: '/', component: VehicleList},
+        {path: '/vehicle/:vehicle_id', component: VehicleView, name: 'vehicle-view'},
+        {path: '/vehicle/add', component: VehicleAdd},
+        {path: '/vehicle/edit/:vehicle_id', component: VehicleEdit, name: 'vehicle-edit'},
+        {path: '/vehicle/edit/:vehicle_id', component: VehicleDelete, name: 'vehicle-delete'}
+    ]
+});
+
+const ClientList = new Vue({
+    el: 'add-client-owner',
+    data: () => ({
+            clients: []
+        }),
+    methods: {
+        async getClients() {
+            await VehicleService.listClients(r => {
+                this.clients = r.data;
+                clients = r.data
+            })
+        }
+    },
+    created() {
+        this.getClients;
+    }
 })
 
 new Vue({
-  router
-}).$mount('#app');
+    el: '#app',
+    data: {
+        company: "Oficina Santa Cruz",
+        title: "Vehicle Managment",
+        subtitle: ""
+    },
+    router: router
+});
